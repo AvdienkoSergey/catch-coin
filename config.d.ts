8f8b2976b7274bd905432fedbe163476af7ad8ce
Sergey Avdienko <avdienkos@icloud.com>
2025-04-05 12:02:25 +0500

    feat(configuration app): Created required d.ts files. Added base app config

---
+++ config.d.ts
@@ -0,0 +1,27 @@
+declare const config: {
+    static: {
+      port: number;
+    };
+    api: {
+      port: number;
+      transport: 'http' | 'https';
+    };
+    sandbox: {
+      timeout: number;
+      displayErrors: boolean;
+    };
+    db: {
+      host: string;
+      port: number;
+      database: string;
+      user: string;
+      password: string;
+    };
+    pg: {
+      database: string;
+      user: string;
+      password: string;
+    };
+  };
+
+  export default config;
---
+++ config.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+    static: {
+      port: process.env.STATIC_PORT,
+    },
+    api: {
+      port: process.env.API_PORT,
+      transport: process.env.API_TRANSPORT,
+    },
+    sandbox: {
+      timeout: process.env.SANDBOX_TIMEOUT,
+      displayErrors: process.env.SANDBOX_DISPLAY_ERRORS,
+    },
+    db: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pg: {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+  };
+  
+  export default config;
