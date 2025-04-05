603265f53259f5f4ac7b015b0adb0edf8699b68d
Sergey Avdienko <avdienkos@icloud.com>
2025-04-05 17:22:41 +0500

    chore(postgres): Added postgres

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 import config from './config.js';
 import logger from './lib/logger.js';
+import pkg from 'pg';
+const { Client } = pkg;
 
-logger.dir(config);
+const client = new Client({
+  host: config.db.host,
+  port: config.db.port,
+  database: config.pg.database,
+  user: config.pg.user,
+  password: config.pg.password
+});
+
+client.connect()
+  .then(() => logger.log('Connected to database!'))
+  .catch(err => logger.error(err))
+  .finally(() => client.end());
